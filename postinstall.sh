fb3ceb8f29b1ffdebfa913e2e744f1bb88161333
Marco Tulio <marcotulionote@gmail.com>
2021-04-03 14:46:29 -0300

    updated config

--- postinstall.sh
+++ postinstall.sh
@@ -15,11 +15,10 @@ sudo apt install python3 python-pip wine nautilus-dropbox docker docker-compose 
 ## installing snap packages
 
 sudo snap install skype --classic &&
-sudo snap install atom --classic &&
 sudo snap install insomnia &&
 sudo snap install spotify &&
 sudo snap install wps-office-multilang &&
-sudo snap install photogimp &&
+sudo snap install code --classic &&
 sudo snap install mailspring &&
 sudo snap install flameshot &&
 
@@ -42,6 +41,8 @@ sudo flatpak install flathub com.anydesk.Anydesk -y &&
 cd ~/Downloads/ && wget -c https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb && wget -c http://media.steampowered.com/client/installer/steam.deb && wget -c https://downloads.mongodb.com/compass/mongodb-compass_1.25.0_amd64.deb && sudo dpkg -i *.deb &&
 
 wget -c https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh && chmod +x ./install.sh && ./install.sh &&
+
+sudo flatpak install flathub org.gimp.GIMP -y && wget -c https://doc-0s-1g-docs.googleusercontent.com/docs/securesc/ha0ro937gcuc7l7deffksulhg5h7mbp1/0v83rmt4mij9897co9ufvor2r1jcj1am/1567965600000/07452089978596344616/*/12i-ihCDSZelx30-oNHJaKAzUei1etsbS?e=download && unzip 12i-ihCDSZelx30-oNHJaKAzUei1etsbS?e=download && cd "PHOTOGIMP V2018 - DIOLINUX" && cd "PATCH" && mkdir -p /home/$USER/.var/app/org.gimp.GIMP/config/GIMP/2.10/ && cp -R * /home/$USER/.var/app/org.gimp.GIMP/config/GIMP/2.10/ &&
 
 ## Updates
 
